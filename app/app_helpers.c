fe58093c1eee05d68ea71c7273162fa0f12febb7
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2025-12-31 10:38:55 -0800

    Changed back to OsOpenFileMode_Write in AppSaveRecentFilesList since this was actually the behavior we desired but the Linux implentation in PigCore os_file.h was mismatched with the behavior on Windows. Since that has been fixed we can go back to the mode we originally had that will create a new file if needed or truncate the existing file. OsOpenFileMode_Create will ONLY create a file, it will not truncate an existing file.

--- app/app_helpers.c
+++ app/app_helpers.c
@@ -460,7 +460,7 @@ void AppSaveRecentFilesList()
 	
 	
 	OsFile fileHandle = ZEROED;
-	if (OsOpenFile(scratch, savePath, OsOpenFileMode_Create, false, &fileHandle))
+	if (OsOpenFile(scratch, savePath, OsOpenFileMode_Write, false, &fileHandle))
 	{
 		VarArrayLoop(&app->recentFiles, rIndex)
 		{
